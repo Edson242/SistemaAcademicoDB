8416b4e0be34bfade91d481e8b9a03cae5513f8d
Edson Silveira <edsonsilveira619@gmail.com>
2025-07-02 11:30:59 -0300

    Alterações no banco de dados

--- banco_gaia.sql
+++ banco_gaia.sql
@@ -2,7 +2,6 @@
 -- Criação do banco de dados
 CREATE DATABASE sistema_academico_integrado;
 \c sistema_academico_integrado;
-
 -- Tabela: Escola
 CREATE TABLE Escola (
     idesc SERIAL PRIMARY KEY,
@@ -17,9 +16,9 @@ CREATE TABLE Aluno (
     nomalu VARCHAR(100) NOT NULL,
     CPFalu VARCHAR(14) UNIQUE NOT NULL,
     emailalu VARCHAR(100) UNIQUE NOT NULL,
-    idesc INT NOT NULL,
-    idhist BIGINT,
-    FOREIGN KEY (idesc) REFERENCES Escola(idesc) ON DELETE CASCADE
+    idesc BIGINT UNSIGNED NOT NULL,
+    idhist BIGINT UNSIGNED,
+    FOREIGN KEY (idesc) REFERENCES Escola(idesc) ON DELETE cascade
 );
 
 -- Tabela: Disciplina
@@ -27,11 +26,19 @@ CREATE TABLE Disciplina (
     id_dis SERIAL PRIMARY KEY,
     nomdis VARCHAR(100) NOT NULL,
     perdis VARCHAR(40),
-    idesc INT NOT NULL,
-    idalu INT,
-    FOREIGN KEY (idesc) REFERENCES Escola(idesc) ON DELETE CASCADE,
-    FOREIGN KEY (idalu) REFERENCES Aluno(idalu)
+    idesc BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (idesc) REFERENCES Escola(idesc) ON DELETE CASCADE
 );
+
+-- Tabela: Aluno_Disciplina
+CREATE TABLE Aluno_Disciplina (
+    idalu BIGINT UNSIGNED NOT NULL,
+    id_dis BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idalu, id_dis),
+    FOREIGN KEY (idalu) REFERENCES Aluno(idalu) ON DELETE CASCADE,
+    FOREIGN KEY (id_dis) REFERENCES Disciplina(id_dis) ON DELETE CASCADE
+);
+
 
 -- Tabela: Professor
 CREATE TABLE Professor (
@@ -39,22 +46,28 @@ CREATE TABLE Professor (
     nomepro VARCHAR(100) NOT NULL,
     emailpro VARCHAR(100) UNIQUE NOT NULL,
     cpfpro VARCHAR(14) UNIQUE NOT NULL,
-    idesc INT NOT NULL,
-    id_dis INT,
-    idalu INT,
+    idesc BIGINT UNSIGNED NOT NULL,
     FOREIGN KEY (idesc) REFERENCES Escola(idesc),
-    FOREIGN KEY (id_dis) REFERENCES Disciplina(id_dis),
-    FOREIGN KEY (idalu) REFERENCES Aluno(idalu)
 );
+
+-- Tabela: Professor_Escola
+CREATE TABLE Professor_Escola (
+    idpro BIGINT UNSIGNED NOT NULL,
+    idesc BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idpro, idesc),
+    FOREIGN KEY (idpro) REFERENCES Professor(idpro) ON DELETE CASCADE,
+    FOREIGN KEY (idesc) REFERENCES Escola(idesc) ON DELETE CASCADE
+);
+
 
 -- Tabela: Matricula
 CREATE TABLE Matricula (
     idmat SERIAL PRIMARY KEY,
     anosem VARCHAR(6) NOT NULL CHECK (char_length(anosem) = 6),
     datmat DATE NOT NULL,
-    idalu INT NOT NULL,
-    idesc INT NOT NULL,
-    idpro INT NOT NULL,
+    idalu BIGINT UNSIGNED NOT NULL,
+    idesc BIGINT UNSIGNED NOT NULL,
+    idpro BIGINT UNSIGNED NOT NULL,
     FOREIGN KEY (idalu) REFERENCES Aluno(idalu),
     FOREIGN KEY (idesc) REFERENCES Escola(idesc),
     FOREIGN KEY (idpro) REFERENCES Professor(idpro)
@@ -62,14 +75,14 @@ CREATE TABLE Matricula (
 
 -- Tabela: Historico
 CREATE TABLE Historico (
-    idhist BIGSERIAL PRIMARY KEY,
+    idhist SERIAL PRIMARY KEY,
     anosem INT NOT NULL,
     nota_final INT CHECK (nota_final BETWEEN 0 AND 100),
     faltas INT CHECK (faltas >= 0),
     percentual_presenca INT CHECK (percentual_presenca BETWEEN 0 AND 100),
     status INT CHECK (status IN (0, 1)), -- 1 = Aprovado, 0 = Reprovado
-    idalu2 INT NOT NULL,
-    id_dis INT NOT NULL,
-    FOREIGN KEY (idalu2) REFERENCES Aluno(idalu),
+    idalu BIGINT UNSIGNED NOT NULL,
+    id_dis BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (idalu) REFERENCES Aluno(idalu),
     FOREIGN KEY (id_dis) REFERENCES Disciplina(id_dis)
 );
